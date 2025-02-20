f96e768829e3e2b4295a0e28317e9d19db7cd54d
Aroush Qureshi <aroushq1@gmail.com>
2025-02-19 18:59:01 -0700

    django & mysql connection setup

--- stylemate/app/app/settings.py
+++ stylemate/app/app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "stylemate_database",
+        "USER": "root",
+        "PASSWORD": "new_password",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
