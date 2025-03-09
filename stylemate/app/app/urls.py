2d67f02a5778dcdaeec2b8f3f58e59373591400f
Aaryan Dhand <aaryandhand@gmail.com>
2025-03-09 16:34:20 -0600

    finished outfit sets and saved outfits

--- stylemate/app/app/urls.py
+++ stylemate/app/app/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("api/login/", views.api_login, name="api_login"),
     path("api/register/", views.api_register, name="api_register"),
+    path('api/add_to_closet/', views.add_to_closet, name='add_to_closet'),
+    path('api/add_saved_outfit/', views.add_saved_outfit, name='add_saved_outfit'),
+
+
 ]
