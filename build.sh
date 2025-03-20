25f520ff87d19835297bdb5307460d906640a737
DeboDam <Debojeetdam.business@gmail.com>
2025-03-19 21:35:20 -0600

    Moving render file to root dir

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Go to backend directory
+cd stylemate/app
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
